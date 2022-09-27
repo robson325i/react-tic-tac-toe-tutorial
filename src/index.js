04a063687a85312523ef189431f3e9b964a8809f
Robson Bromberg dos Santos <robsonbromberg@gmail.com>
2022-09-26 22:03:30 -0300

    refactoring the board and squares in separate components jsx

--- src/index.js
+++ src/index.js
@@ -1,139 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import io from "socket.io-client";
+import Board from "./components/Board";
 import "./index.css";
 
-function Square(props) {
+function Game() {
+  // Game Init, estabilish connection with server.
+  const socket = io('http://localhost:3001');
+  socket.on('connect', () => {
+    console.log('Socket Connected.');
+  });
+
   return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
-      {props.value}
-    </button>
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      {/* <div className="game-info">
+        <div></div>
+        <ol></ol>
+      </div> */}
+    </div>
   );
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      player: "X",
-    };
-  }
-
-  calculateWinner() {
-    const squares = this.state.squares;
-    const lines = [
-      // Horizontals
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      // Verticals
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      // Diagonals
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  calculateEnd() {
-    const squares = this.state.squares;
-    for (let i = 0; i < squares.length; i++) {
-      if (squares[i] === null) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    const winner = this.calculateWinner();
-    if (winner || squares[i]) {
-      return;
-    }
-    if (this.state.player === "X") {
-      squares[i] = "X";
-      this.setState({ player: "O" });
-    } else {
-      squares[i] = "O";
-      this.setState({ player: "X" });
-    }
-    this.setState({ squares: squares });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const winner = this.calculateWinner();
-    const end = this.calculateEnd();
-    let status;
-
-    status = "Jogador atual: " + this.state.player;
-    
-    if (end) {
-      status = "Empate.";
-    }
-
-    if (winner) {
-      status = "Vencedor: " + winner;
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        {/* <div className="game-info">
-          <div></div>
-          <ol></ol>
-        </div> */}
-      </div>
-    );
-  }
 }
 
 // ========================================
